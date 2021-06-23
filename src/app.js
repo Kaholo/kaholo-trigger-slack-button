13c658be306465e51548b64388a9756e41ef1b13
Amit Alon <amit@kaholo.io>
2021-06-23 17:07:06 +0300

    refactored to new format

--- src/app.js
+++ src/app.js
@@ -1,37 +1,31 @@
-const { findTriggers } = require(`./helpers`);
+const { verifySignature } = require(`./helpers`);
 const minimatch = require("minimatch");
 
-async function webhookButton(req, res) {
+async function webhookButton(req, res, settings, triggerControllers) {
+  if (!triggerControllers) {
+    return res.status(400).send("triggers cannot be nil");
+  }
   if (req.body.challenge){
-    return res.send(req.body.challenge);
+    return res.status(200).send(req.body.challenge);
   }
-  const body = JSON.parse(req.body.payload);
-  
-  if (body.type !== "interactive_message"){
-    res.send("ERROR");
-    throw "not an interactive message";
+  try {
+    const body = JSON.parse(req.body.payload);
+    if (body.type !== "interactive_message") return res.status(400).send("not an interactive message");
+    const buttons = body.actions.filter(act => act.type === "button");
+    
+    triggerControllers.forEach(trigger => {
+      if (!verifySignature(req, trigger)) return;
+      const {valuePat} = trigger.params;
+      buttons.forEach(button => {
+        if (valuePat && !minimatch(button.value, valuePat)) return;
+        trigger.execute(`Slack Button "${button.value}"`, button);
+      })
+    });
+    res.status(200).send("OK");
   }
-  const btnValues = body.actions.filter(act => act.type === "button").map(act => act.value);
-
-  findTriggers(
-    validateTrigger,
-    btnValues,
-    body,
-    req, res,
-    "webhookButton",
-    `buttons ${btnValues.join(", ")}`
-  );
-}
-
-async function validateTrigger(trigger, btnValues) {
-  const valPat = (trigger.params.find((o) => o.name === `valuePat`).value || "").trim();
-
-  // Check if the value pattern was provided, and if so check it matches request
-  if (valPat && !btnValues.some(val => minimatch(val, valPat))) {
-    throw `No matching button value`;
+  catch (err){
+    res.status(422).send(err.message);
   }
-
-  return true;
 }
 
 module.exports = { 
